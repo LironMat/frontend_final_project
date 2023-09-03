849b6bf83f2c5a558ae9a543ccdfa138ae1f8a29
Tzach Fleischer <tzahflisher@gmail.com>
2023-09-03 17:48:46 +0300

    fixed month and year input

--- src/AddItemForm/AddItemForm.js
+++ src/AddItemForm/AddItemForm.js
@@ -3,16 +3,13 @@ import {useState} from "react";
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import Select from '@mui/material/Select';
-import {FormLabel, useTheme} from "@mui/material";
 import OutlinedInput from '@mui/material/OutlinedInput';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import { IDB } from '../idbModule';
 
-//{ sum: 200, category: 'HOUSING', description: 'rent for 01.2023', month: 7, year: 2022 }
 const categories = ['FOOD', 'HEALTH', 'EDUCATION', 'TRAVEL', 'HOUSING', 'OTHER'];
-const months = [1,2,3,4,5,6,7,8,9,10,11,12];
 
 const ITEM_HEIGHT = 48;
 const ITEM_PADDING_TOP = 8;
@@ -25,34 +22,22 @@ const MenuProps = {
     },
 };
 
-function getStyles(category, categoryName, theme) {
-    return {
-        fontWeight:
-            categoryName.indexOf(category) === -1
-                ? theme.typography.fontWeightRegular
-                : theme.typography.fontWeightMedium,
-    };
-}
 
 
 
 export default function AddItemForm() {
 
-    const theme = useTheme();
     const[sumInput,setSumInput] = useState(0);
     const[categoryInput,setCategoryInput] = useState("FOOD");
     const[descriptionInput,setDescriptionInput] = useState(" ");
     const[monthInput,setMonthInput] = useState(undefined);
     const[yearInput,setYearInput] = useState(undefined);
+    const[yearMonthValue,setYearMonthValue] = useState("2023-09");
 
     async function handleAddItem() {
         /**
          * @type {DB}
          */
-        if(monthInput === undefined || yearInput === undefined) {
-            setMonthInput(undefined);
-            setYearInput(undefined);
-        }
 
         const db = await IDB.openCostsDB('costsdb', 1);
 
@@ -63,43 +48,33 @@ export default function AddItemForm() {
             month: monthInput,
             year: yearInput
         });
-        console.log(sumInput);
-        console.log(categoryInput);
-        console.log(descriptionInput);
+
         if(result) {
-            alert("item added succesfully");
+            alert("item added successfully");
         }
         else {
             alert("error adding item");
         }
 
-        //const result2 = await db.addCost({sum: sumInput, category: categoryInput, description: descriptionInput});
     }
 
     function handleSumChange(e) {
-        //console.log(e.target.value);
         setSumInput(parseInt(e.target.value));
     }
 
     function handleDescriptionChange(e) {
-        //console.log(e.target.value);
         setDescriptionInput(e.target.value);
     }
 
     function handleCategoryChange(e) {
-        console.log(e.target.value);
         setCategoryInput(e.target.value);
     }
-    function handleMonthChange(e) {
-        console.log(e.target.value);
-        setMonthInput(e.target.value);
-    }
 
-    function handleYearChange(e) {
-        if(parseInt(e.target.value)) {
-            setYearInput(parseInt(e.target.value));
-        }
-
+    function handleYearMonthChange(e) {
+        let yearMonthList = (e.target.value).split("-");
+        setYearInput(parseInt(yearMonthList[0]));
+        setMonthInput(parseInt(yearMonthList[1]));
+        setYearMonthValue(e.target.value);
     }
 
   return(<div>
@@ -148,34 +123,13 @@ export default function AddItemForm() {
       </Select>
           <br/>
       </FormControl>
-          <FormControl normal>
-              <InputLabel id="month-select-component">Month</InputLabel>
-              <Select
-                  labelId="month-select-component"
-                  id="month-select-component"
-                  value={monthInput}
-                  onChange={handleMonthChange}
-                  input={<OutlinedInput label="Month" />}
-                  MenuProps={MenuProps}
-              >
-                  {months.map((monthNumber) => (
-                      <MenuItem
-                          key={monthNumber}
-                          value={monthNumber}
-                          //style={getStyles(categoryName, category, theme)}
-                      >
-                          {monthNumber}
-                      </MenuItem>
-                  ))}
-              </Select>
-          </FormControl>
           <br/>
           <TextField
-              value={yearInput}
-              onChange={handleYearChange}
+              value={yearMonthValue}
+              onChange={handleYearMonthChange}
               id="outlined-year"
               label="Year"
-              type="number"
+              type="month"
               InputLabelProps={{
                   shrink: true,
               }}
@@ -184,7 +138,6 @@ export default function AddItemForm() {
       </FormControl>
           
           <Button onClick={(e) => handleAddItem()} size="small" variant="outlined">Add Item</Button>
-
 
       </div>);
 }
